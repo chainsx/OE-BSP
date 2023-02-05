240542b99215c5683ca185f1f8b804252363ad6c
chainsx <pzhrhlx@outlook.com>
2023-02-05 23:04:06 +0800

    Update extlinux.sh

--- config/u-boot/boot-method/extlinux.sh
+++ config/u-boot/boot-method/extlinux.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-source ./scripts/common.sh
-
 root_dev=$1
 boot_dev=$2
+CMELINE=$3
+BOOT_DTB_FILE=$4
 
 apply_boot-method(){
     line=$(blkid | grep ${root_dev})
     uuid=${line#*UUID=\"}
-    uuid=${uuid%%\"*}
-    LOG "root-UUID=${uuid}"
-    LOG "CMDLINE=${CMDLINE}"
-    LOG "BOOT_DTB_FILE=${BOOT_DTB_FILE}"
-
+    uuid={uuid%%\"*}
     mkdir -p ${boot_dev}/extlinux
     echo "label openEuler
     kernel /Image
@@ -20,6 +16,4 @@ apply_boot-method(){
     fdt /dtb/${BOOT_DTB_FILE}
     append  root=UUID=${uuid} ${CMDLINE}" > ${boot_dev}/extlinux/extlinux.conf
 }
-
-check_and_apply_board_config
 apply_boot-method
